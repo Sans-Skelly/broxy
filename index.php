131d70961464b4e00a16395ccfb5a2d2d4b49b4e
Berke Emrecan ARSLAN <beremaran@gmail.com>
2017-05-09 21:19:59 +0300

    Added License

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: beremaran
- * Date: 09.05.2017
- * Time: 14:21
+MIT License
+
+Copyright (c) 2017 Berke Emrecan ARSLAN
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
  */
 
 session_start();
 
 $PRINT_HTML = false;
-$REMOTE_ADDRESS = "beremaran.com";
-$PROXY_ADDRESS = "localhost:8090";
+$REMOTE_ADDRESS = "beremaran.com"; // address to be proxified
+$PROXY_ADDRESS = "localhost:8090"; // address of Broxy script
 
 //$ESCAPED_REMOTE_ADDRESS = str_replace(".", "\\.", $REMOTE_ADDRESS);
 
